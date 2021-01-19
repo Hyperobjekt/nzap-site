8d1dd208faa61050a2e48773686702ca9367c7e8
Uri Maxima <uri@maxima.ninja>
2021-01-19 10:53:56 -0600

    style above the fold homepage

--- src/components/_global/Header.js
+++ src/components/_global/Header.js
@@ -7,18 +7,25 @@ const Header = () => {
   const sg = styles();
   const activeStyle = { color: sg.colors.primary };
   return (
-    <div className="container">
-      <div className="row pt-5 pb-5">
-        <div className="col-4 project-title">
-          <NavLink className="pr-2 pl-2" to="/" exact>NET-ZERO AMERICA</NavLink>
-        </div>
-        <div className="col-8 text-right">
-          <nav>
-            <NavLink className="pr-2 pl-2" to="/#explore" activeStyle={activeStyle} exact>Explore the Data</NavLink>
-            <NavLink className="pr-2 pl-2" to="/publications" activeStyle={activeStyle} exact>Publications</NavLink>
-            <NavLink className="pr-2 pl-2" to="/media" activeStyle={activeStyle} exact>Media</NavLink>
-            <NavLink className="pl-2" to="/about" activeStyle={activeStyle} exact>About</NavLink>
-          </nav>
+    <div className="container-fluid nzap-header">
+      <div className="row">
+        <div className="col-12">
+          <div className="container">
+            <div className="row pt-2 pb-5">
+              <div className="col-4 project-title">
+                <NavLink className="pr-2 pl-2" to="/" exact>NET-ZERO AMERICA</NavLink>
+              </div>
+              <div className="col-8 text-right">
+                <nav className="nzap-navigation">
+                  <NavLink className="pr-2 pl-2" to="/#explore" activeStyle={activeStyle} exact>Explore the Data</NavLink>
+                  <NavLink className="pr-2 pl-2" to="/publications" activeStyle={activeStyle} exact>Publications</NavLink>
+                  <NavLink className="pr-2 pl-2" to="/media" activeStyle={activeStyle} exact>Media</NavLink>
+                  <NavLink className="pl-2" to="/about" activeStyle={activeStyle} exact>About</NavLink>
+                </nav>
+              </div>
+            </div>
+
+          </div>
         </div>
       </div>
     </div>
