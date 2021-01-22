066e9e0174166a46f4aa7a70379c1822174d1aa0
Uri Maxima <uri@maxima.ninja>
2021-01-22 15:38:06 -0600

    Update landing page copy

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -33,8 +33,8 @@ const HomePage = () => (
               <div className="d-block display-3 text-uppercase pt-md-5 mt-md-f5 title">Net-Zero America:</div>
               <div className="d-block lead display-4 subtitle">Potential Pathways, <br />Infrastructure, and Impacts</div>
               <div className="d-block pr-3 pt-4 introduction">
-                <p className="pt-4">Using state-of-the-art modeling tools, a new study from Princeton’s Andlinger Center For Energy + The Environment provides <span className="highlight">five plausible scenarios for the U.S. to reach net-zero emissions by 2050</span>, each one featuring historically unprecedented rates of deployment for multiple technologies.</p>
-                <p className="pt-2">This site presents these scenarios in an interactive context so that policy makers, experts and other interested parties can examine approaches to decarbonization identified by the Net-Zero America Project.</p>
+                <p className="pt-4">The Net-Zero America research quantifies five distinct technological pathways, all using technologies known today, by which the United States could decarbonize its entire economy. The study is the first to map with high geographic specificity the infrastructure that needs to be built and the investments required to run the country without emitting more greenhouse gases into the atmosphere than are removed from it each year. It quantifies inherent challenges and opportunities with different pathways, and the granularity allows state-level assessments of implications for land use, employment, air pollution-related health impacts, capital mobilization, incumbent fossil fuel industries, and new clean-energy industries.</p>
+                <p className="pt-2">With multiple plausible and affordable pathways available, the societal conversation can now turn from “if” to “how” and focus on the choices the nation and its myriad stakeholders wish to make to shape the energy transition. Insights from this study can inform this conversation.  This website presents the pathways in an interactive context to enable policy makers and other stakeholders to extract specific results that are most useful to them. The site should be used in conjunction with the Net-Zero America final report to fully understand the data contained herein.</p>
               </div>
             </div>
 
@@ -47,9 +47,7 @@ const HomePage = () => (
             </div>
             <div className="d-inline-block quote-holder pl-md-4">
               <div className="d-block quote">
-                “With multiple plausible and affordable pathways available, <br className="d-none d-md-block" />
-                the societal conversation can now turn from ‘if ’ to ‘how’ and <br className="d-none d-md-block" />
-                focus on the choices.”
+                “Net Zero America...sets an entirely new standard in this genre...to provide an unprecedented degree of clarity and granularity about possible pathways to mid-century “net zero” for this country…. Everybody seriously interested in the crucial question of this country’s energy-climate future—not least the new Biden-Harris administration—needs to understand the findings of this extraordinary study.”
               </div>
               <div className="d-block pt-2 by">
                 <span><b>John P. Holdren,</b> <br className="d-md-none d-block" />Science Advisor to President Obama</span>
@@ -76,11 +74,9 @@ const HomePage = () => (
               <span className="highlight">E+</span><span className="pl-1 label">High<br />Electrification</span>
             </h4>
             <ul className="features ml-0 pl-3 pb-4">
-              <li>Lorem ipsum</li>
-              <li>1.011 Dolor sit amet</li>
-              <li>Consectetuer</li>
-              <li>10% Adipiscing elit</li>
-              <li>Sed diam nonummy</li>
+              <li>Nearly full electrification of transport and buildings by 2050</li>
+              <li>No land-use change for biomass supply allowed</li>
+              <li>Few other constraints on energy supply options</li>
             </ul>
           </div>
           <div className="col-md-4 pr-3 approach">
@@ -92,11 +88,9 @@ const HomePage = () => (
               <span className="highlight">E-</span><span className="pl-1 label">Less-High<br />Electrification</span>
             </h4>
             <ul className="features ml-0 pl-3 pb-4">
-              <li>Lorem ipsum</li>
-              <li>1.011 Dolor sit amet</li>
-              <li>Consectetuer</li>
-              <li>10% Adipiscing elit</li>
-              <li>Sed diam nonummy</li>
+              <li>Less-rapid electrification of transport and buildings</li>
+              <li>No land-use change for biomass supply allowed</li>
+              <li>Few other constraints on energy supply options</li>
             </ul>
           </div>
           <div className="col-md-4 pr-3 approach">
@@ -105,14 +99,12 @@ const HomePage = () => (
             </div>
             <h6 className="scenario pt-2">Scenario 3</h6>
             <h4 className="title">
-              <span className="highlight">E-B+</span><span className="pl-1 label">High<br />Biomass</span>
+              <span className="highlight">E- B+</span><span className="pl-1 label">High<br />Biomass</span>
             </h4>
             <ul className="features ml-0 pl-3 pb-4">
-              <li>Lorem ipsum</li>
-              <li>1.011 Dolor sit amet</li>
-              <li>Consectetuer</li>
-              <li>10% Adipiscing elit</li>
-              <li>Sed diam nonummy</li>
+              <li>Less-rapid electrification of transport and buildings</li>
+              <li>Biomass supply requires converting some agricultural land from food to energy crops</li>
+              <li>Few other constraints on energy supply options</li>
             </ul>
           </div>
           <div className="col-md-4 pr-3 approach">
@@ -121,13 +113,13 @@ const HomePage = () => (
             </div>
             <h6 className="scenario pt-2">Scenario 4</h6>
             <h4 className="title">
-              <span className="highlight">E+RE-</span><span className="pl-1 label">Renewable<br />Constrained</span>
+              <span className="highlight">E+ RE-</span><span className="pl-1 label">Renewable<br />Constrained</span>
             </h4>
             <ul className="features ml-0 pl-3 pb-4">
-              <li>Lorem ipsum</li>
-              <li>1.011 Dolor sit amet</li>
-              <li>Consectetuer</li>
-              <li>10% Adipiscing elit</li>
+              <li>Nearly full electrification of transport and buildings by 2050</li>
+              <li>Solar and wind power annual capacity additions constrained to historical maximum</li>
+              <li>No land-use change for biomass supply allowed</li>
+              <li>Few other constraints on energy supply options</li>
               <li>Sed diam nonummy</li>
             </ul>
           </div>
@@ -137,14 +129,14 @@ const HomePage = () => (
             </div>
             <h6 className="scenario pt-2">Scenario 5</h6>
             <h4 className="title">
-              <span className="highlight">E+RE+</span><span className="pl-1 label">100%<br />Renewable</span>
+              <span className="highlight">E+ RE+</span><span className="pl-1 label">100%<br />Renewable</span>
             </h4>
             <ul className="features ml-0 pl-3 pb-4">
-              <li>Lorem ipsum</li>
-              <li>1.011 Dolor sit amet</li>
-              <li>Consectetuer</li>
-              <li>10% Adipiscing elit</li>
-              <li>Sed diam nonummy</li>
+              <li>Nearly full electrification of transport and buildings by 2050</li>
+              <li>No fossil fuel use allowed by 2050</li>
+              <li>No land-use change for biomass supply allowed</li>
+              <li>No new nuclear power construction allowed, existing plants retired</li>
+              <li>No underground storage of CO2 allowed</li>
             </ul>
           </div>
         </div>
