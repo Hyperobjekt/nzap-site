c51ee3dd9295b5e1bad184fffb5819fa547a5007
Uri Maxima <uri@maxima.ninja>
2021-01-26 13:54:47 -0600

    scaffold Fact sheets

--- src/components/static/FactSheet.js
+++ src/components/static/FactSheet.js
@@ -1,7 +1,75 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import { loadFilters } from '../../redux/actions/FiltersActions';
+import { handleError } from '../../_helpers'
+import introImage from '../../assets/images/the-report/intro-image.jpg';
 
-const FactSheetPage = () => (
-  <>Fact SHeet</>
-);
+import './FactSheet.scss';
 
-export default FactSheetPage;
+const FactSheetPage = ({ filters, loadFilters }) => {
+  useEffect(() => {
+    loadFilters(null).catch(handleError);
+  }, [])
+
+
+  const loadStates = states => {
+    return states.map((state, i) => <div className="col-3 pt-2 pb-2 fact-state" key={i}>
+      <a href="">{state.label}</a>
+    </div>)
+  }
+
+  return <div className="container pb-5">
+    <div className="row">
+      <div className="col-12 position-relative">
+
+        <div className="row">
+          <div className="col-12 pt-5 atf the-report">
+            <div className="row">
+              <div className="col-12 pt-3 pt-md-5 col-md-7 position-relative">
+                <div className="d-block lead display-4 position-absolute subtitle">Fact Sheet</div>
+              </div>
+              <div className="col-12 col-md-5 text-center">
+                <img className="intro-image d-none d-md-inline-block" src={introImage} alt="" />
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-12">
+                <div className="d-block pr-3 pt-4 introduction">
+                  <p className="pt-md-4">
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Architecto, quos dolorum? Repellendus impedit ab cum fugit ea delectus similique, reiciendis autem maxime repudiandae velit quis temporibus sunt nisi cumque laudantium?
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Architecto, quos dolorum?
+                  </p>
+                </div>
+              </div>
+            </div>
+
+          </div>
+        </div>
+
+        <div className="row pt-5 fact-states">
+          {loadStates(filters.usStates)}
+        </div>
+      </div>
+    </div>
+
+
+  </div>
+
+}
+
+
+FactSheetPage.propTypes = {
+  filters: PropTypes.object.isRequired,
+  loadFilters: PropTypes.func.isRequired
+}
+
+function mapStateToProps(state) {
+  return {
+    filters: state.filters
+  }
+}
+
+const mapDispatchToProps = { loadFilters }
+
+export default connect(mapStateToProps, mapDispatchToProps)(FactSheetPage);
--- src/redux/reducers/filtersReducer.js
+++ src/redux/reducers/filtersReducer.js
@@ -2,20 +2,24 @@ import * as types from "../actions/actionTypes";
 import initialState from "./initialState";
 
 const extractL1Filters = action => {
+  if (!action.query) return [...action.filters.levelOneFilters]
   return [...action.filters.levelOneFilters]
     .map(e => ({ ...e, active: action.query.categories.indexOf(e.slug) > -1 }))
 }
 const extractL2Filters = action => {
+  if (!action.query) return [...action.filters.levelOneFilters]
   return [...action.filters.levelOneFilters]
     .filter(e => action.query.categories.indexOf(e.slug) > -1)
     .map(e => e.levelTwoFilters).flat()
     .map(e => ({ ...e, active: action.query.subcategories.indexOf(e.slug) > -1 }))
 }
 const extractUsStates = action => {
-  return action.filters.usStates.map(e => ({ ...e, active: action.query.state === e.slug }))
+  if (!action.query) return [...action.filters.usStates]
+  return [...action.filters.usStates].map(e => ({ ...e, active: action.query.state === e.slug }))
 }
 const extractYears = action => {
-  return action.filters.years.map(e => ({ ...e, active: action.query.year === e.slug }));
+  if (!action.query) return [...action.filters.years]
+  return [...action.filters.years].map(e => ({ ...e, active: action.query.year === e.slug }));
 }
 
 export default function filtersReducer(state = initialState.filters, action) {
