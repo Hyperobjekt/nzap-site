e33eda3cb5ac4ded7c98764962f0f5772d4f4a09
James Minton <james-minton@github.com>
2021-02-04 17:41:11 -0500

    Update media page

--- src/components/static/Media.js
+++ src/components/static/Media.js
@@ -6,6 +6,7 @@ import theInterchangeLogo from '../../assets/images/media/the-interchange.png';
 import theNewYorkTimesLogo from '../../assets/images/media/the-new-york-times.png';
 import theWashingtonPostLogo from '../../assets/images/media/the-washington-post.png';
 import yahooFinanceLogo from '../../assets/images/media/yahoo-finance.png';
+import gristLogo from '../../assets/images/media/media-logos-grist.png';
 
 import './Media.scss';
 
@@ -24,7 +25,7 @@ const MediaPage = () => (
                 <img className="intro-image d-none d-md-inline-block" src={introImage} alt="" />
               </div>
             </div>
-            <div className="row">
+            <div className="row d-none">
               <div className="col-12">
                 <div className="d-block pr-3 pt-4 introduction">
                   <p className="pt-md-4">
@@ -41,48 +42,62 @@ const MediaPage = () => (
       </div>
     </div>
 
-    <div className="row name-drop">
-      <div className="col-4 pb-3 pt-5">
+    <div className="row name-drop pt-2">
+      <div className="col-12 col-md-4 pb-3 pt-5">
         <div className="image">
           <img className="w-100" src={theNewYorkTimesLogo} alt="" />
         </div>
-        <div className="pt-3 pb-3 publication">New York Times</div>
-        <div className="snippet">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum culpa molestias excepturi doloribus error at quae natus quasi.</div>
+        <div className="pt-3 pb-2 publication">New York Times</div>
+        <div className="snippet">&#8220;To Cut Emissions to Zero, U.S. Needs to Make Big Changes in Next 10 Years&#8221;</div>
+        <div className="articleLink"><a href="https://www.nytimes.com/2020/12/15/climate/america-next-decade-climate.html" target="_blank" rel="noopener noreferrer">READ THE ARTICLE</a></div>
       </div>
-      <div className="col-4 pb-3 pt-5">
+      <div className="col-12 col-md-4 pb-3 pt-5">
         <div className="image">
           <img className="w-100" src={theWashingtonPostLogo} alt="" />
         </div>
-        <div className="pt-3 pb-3 publication">Washington Post</div>
-        <div className="snippet">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum culpa molestias excepturi doloribus error at quae natus quasi.</div>
+        <div className="pt-3 pb-2 publication">Washington Post</div>
+        <div className="snippet">&#8220;Biden wants the U.S. to stop contributing to climate change by 2050. Here’s what that would actually take.&#8221;</div>
+        <div className="articleLink"><a href="https://www.washingtonpost.com/climate-environment/2020/12/15/biden-wants-halt-all-us-climate-emissions-by-2050-heres-what-that-would-actually-take/" target="_blank" rel="noopener noreferrer">READ THE ARTICLE</a></div>
       </div>
-      <div className="col-4 pb-3 pt-5">
+      <div className="col-12 col-md-4 pb-3 pt-5">
         <div className="image">
           <img className="w-100" src={fastCompanyLogo} alt="" />
         </div>
-        <div className="pt-3 pb-3 publication">Fast Company</div>
-        <div className="snippet">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum culpa molestias excepturi doloribus error at quae natus quasi.</div>
+        <div className="pt-3 pb-2 publication">Fast Company</div>
+        <div className="snippet">&#8220;The U.S. can get to net-zero emissions by 2050. Here’s how&#8221;</div>
+        <div className="articleLink"><a href="https://www.fastcompany.com/90589586/the-u-s-can-get-to-net-zero-emissions-by-2050-heres-how" target="_blank" rel="noopener noreferrer">READ THE ARTICLE</a></div>
       </div>
-      <div className="col-4 pb-3 pt-5">
+      <div className="col-12 col-md-4 pb-3 pt-5">
         <div className="image">
           <img className="w-100" src={howDoWeFixItLogo} alt="" />
         </div>
-        <div className="pt-3 pb-3 publication">How do we fix it?</div>
-        <div className="snippet">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum culpa molestias excepturi doloribus error at quae natus quasi.</div>
+        <div className="pt-3 pb-2 publication">How do we fix it?</div>
+        <div className="snippet">&#8220;Net-Zero America by 2050: Jesse Jenkins and Eric Larson&#8221;</div>
+        <div className="articleLink"><a href="https://www.howdowefixit.me/new-blog-3/netzerousa" target="_blank" rel="noopener noreferrer">READ THE ARTICLE</a></div>
       </div>
-      <div className="col-4 pb-3 pt-5">
+      <div className="col-12 col-md-4 pb-3 pt-5">
         <div className="image">
           <img className="w-100" src={theInterchangeLogo} alt="" />
         </div>
-        <div className="pt-3 pb-3 publication">The Interchange</div>
-        <div className="snippet">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum culpa molestias excepturi doloribus error at quae natus quasi.</div>
+        <div className="pt-3 pb-2 publication">The Interchange</div>
+        <div className="snippet">&#8220;Paths to Net-Zero Emissions by 2050&#8221;</div>
+        <div className="articleLink"><a href="https://art19.com/shows/the-interchange/episodes/9ff3d1cc-7026-4b99-bdf1-380b4014da64" target="_blank" rel="noopener noreferrer">READ THE ARTICLE</a></div>
       </div>
-      <div className="col-4 pb-3 pt-5">
+      <div className="col-12 col-md-4 pb-3 pt-5">
         <div className="image">
           <img className="w-100" src={yahooFinanceLogo} alt="" />
         </div>
-        <div className="pt-3 pb-3 publication">Yahoo Finance</div>
-        <div className="snippet">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum culpa molestias excepturi doloribus error at quae natus quasi.</div>
+        <div className="pt-3 pb-2 publication">Yahoo Finance</div>
+        <div className="snippet">&#8220;The priorities are doubling down on wind and solar generation and accelerate electric vehicles&#8221;</div>
+        <div className="articleLink"><a href="https://finance.yahoo.com/video/priorities-doubling-down-wind-solar-170022720.html" target="_blank" rel="noopener noreferrer">READ THE ARTICLE</a></div>
+      </div>
+      <div className="col-12 col-md-4 pb-3 pt-5">
+        <div className="image">
+          <img className="w-100" src={gristLogo} alt="" />
+        </div>
+        <div className="pt-3 pb-2 publication">Grist</div>
+        <div className="snippet">&#8220;The priorities are doubling down on wind and solar generation and accelerate electric vehicles&#8221;</div>
+        <div className="articleLink"><a href="https://finance.yahoo.com/video/priorities-doubling-down-wind-solar-170022720.html" target="_blank" rel="noopener noreferrer">READ THE ARTICLE</a></div>
       </div>
     </div>
 
