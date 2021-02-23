4e52adc86cc43278da2162d7601664fa7caa5a1c
Uri Maxima <uri@maxima.ninja>
2021-02-22 20:43:44 -0700

    The older child subcategory stays active in the URL and it prevents the view from updating

--- src/redux/reducers/filtersReducer.js
+++ src/redux/reducers/filtersReducer.js
@@ -1,12 +1,23 @@
 import * as types from "../actions/actionTypes";
 import initialState from "./initialState";
-import { assembleFilters } from '../../_helpers'
+import { assembleFilters, generateUrl } from '../../_helpers'
 
 
+const resetLevelTwo = filters => {
+  let activeCategories = [...filters.levelOneFilters].filter(e => e.active).map(e => e.slug);
+  let levelTwoFilters = [...filters.levelTwoFilters].map(e => {
+    let subcategory = { ...e }
+    if (!activeCategories.includes(e.levelOneSlug)) { subcategory.active = false }
+    return subcategory;
+  })
+  let newFilters = { ...filters, levelTwoFilters }
 
+  return { ...newFilters, url: generateUrl(newFilters) }
+
+}
 
 const setFilters = (stateFilters, actionFilters) => {
-  return { ...stateFilters, ...actionFilters }
+  return { ...stateFilters, ...resetLevelTwo(actionFilters) }
 }
 
 export default function filtersReducer(state = initialState.filters, action) {
